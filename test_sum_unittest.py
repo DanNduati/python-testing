fb132ba89697fc4db301870372da90b8e5b67e8b
DanielNduati <dannduati2@gmail.com>
2022-06-03 20:55:55 +0300

    Add test function and test the function raises an exception

--- test_sum_unittest.py
+++ test_sum_unittest.py
@@ -1,12 +1,25 @@
 import unittest
 
+#function to test
+def add_iter(obj):
+    if len(obj)>3:
+        #add only 3 items
+        raise ValueError("Limit reached")
+    return sum(obj)
+
 class TestSum(unittest.TestCase):
     #put tests into classes as methods
     def test_sum(self):
-        self.assertEqual(sum([1,2,3]),6,"Should be 6")
+        self.assertEqual(add_iter([1,2,3]),6,"Should be 6")
         
     def test_sum_tuple(self):
-        self.assertEqual(sum((1,2,2)),6,"Should be 5")
-        
+        self.assertEqual(add_iter((1,2,2)),6,"Should be 5")
+    
+    def test_sum_exception(self):
+        too_many_items = [1,2,3,4]
+        with self.assertRaises(ValueError) as exception_context:
+            add_iter(too_many_items)
+        self.assertEqual(str(exception_context.exception),"Limit reached")
+
 if __name__ == "__main__":
     unittest.main()
