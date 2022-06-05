790e038046cfae701a61c5fc9415d7a23f98fa60
DanielNduati <dannduati2@gmail.com>
2022-06-05 12:08:32 +0300

    Add more assertion tests to test suite

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -3,3 +3,15 @@ def test_always_pass():
     
 def test_always_fail():
     assert(False)
+    
+def test_capitalize():
+    assert("daniel".capitalize() == "Daniel")
+    
+def test_reversed():
+    assert(list(reversed([1,2,3,4]))==[4,3,2,1])
+    
+def test_some_primes():
+    assert 37 in {
+        num for num in range(1,50)
+        if num !=1 and not any([num%div ==0 for div in range(2,num)])
+    }
