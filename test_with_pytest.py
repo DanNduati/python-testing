a5128ab539be1a7d308117a4e16d59d311fb9e50
DanielNduati <dannduati2@gmail.com>
2022-06-07 19:22:23 +0300

    Refactor code with black

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -1,17 +1,25 @@
+import pytest
+
+
 def test_always_pass():
     assert True
-    
+
+
 def test_always_fail():
-    assert(False)
-    
+    assert False
+
+
 def test_capitalize():
-    assert("daniel".capitalize() == "Daniel")
-    
+    assert "daniel".capitalize() == "Daniel"
+
+
 def test_reversed():
-    assert(list(reversed([1,2,3,4]))==[4,3,2,1])
-    
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+
 def test_some_primes():
     assert 37 in {
-        num for num in range(1,50)
-        if num !=1 and not any([num%div ==0 for div in range(2,num)])
+        num
+        for num in range(1, 50)
+        if num != 1 and not any([num % div == 0 for div in range(2, num)])
     }
